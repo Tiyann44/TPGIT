ca41f4e66ad02494d6ffad32d0c4b04b4cad3780
Tiyann44 <tiyann2410@gmail.com>
2026-04-30 12:11:51 +0200

    add jeux et console dans le main

--- NintendoProject/src/test/java/nintendo/test/Test.java
+++ NintendoProject/src/test/java/nintendo/test/Test.java
@@ -1,9 +1,21 @@
 package nintendo.test;
 
+import nintendo.model.Console;
+import nintendo.model.Jeu;
+
 public class Test {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		Console nintendoDS = new Console("nintendoDS");
+		Console Wii = new Console("Wii");
+		Console Switch = new Console("Switch");
+		
+		Jeu jeu1 = new Jeu("Dragon Quest IX", nintendoDS);
+		Jeu jeu2 = new Jeu("MarioKart", Wii);
+		Jeu jeu3 = new Jeu("Super Smash Bros", Switch);
+		Jeu jeu4 = new Jeu("Metroid", nintendoDS);
+		Jeu jeu5 = new Jeu("Animal Crossing : New Horizon", Switch);
 
 	}
 
