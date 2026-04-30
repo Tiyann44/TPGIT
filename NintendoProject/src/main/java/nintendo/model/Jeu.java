9503ce9159278c6fbc0958bc29e00b5ec08c7a2f
Cécile <ccilejouanne@yahoo.fr>
2026-04-30 12:30:18 +0200

    Modification classe Jeu: ajout argument Boutique

--- NintendoProject/src/main/java/nintendo/model/Jeu.java
+++ NintendoProject/src/main/java/nintendo/model/Jeu.java
@@ -4,12 +4,14 @@ public class Jeu {
 
 	private String titre;
 	private Console console;
+	private Boutique boutique;
 	
 	
 	
-	public Jeu(String titre, Console console) {
+	public Jeu(String titre, Console console, Boutique boutique) {
 		this.titre = titre;
 		this.console = console;
+		this.boutique = boutique;
 	}
 
 
@@ -36,11 +38,19 @@ public class Jeu {
 		this.console = console;
 	}
 
+	public Boutique getBoutique() {
+		return boutique;
+	}
 
+
+
+	public void setBoutique(Boutique boutique) {
+		this.boutique = boutique;
+	}
 
 	@Override
 	public String toString() {
-		return "Jeu [titre=" + titre + ", console=" + console + "]";
+		return "Jeu [titre=" + titre + ", console=" + console + ", boutique=" + boutique + "]";
 	}
 	
 	
