b11f3817d9ab5840ffea71f26584f83f4b8866c6
Tiyann44 <tiyann2410@gmail.com>
2026-04-30 12:58:44 +0200

    WIP on feature/updateClient
    updateClient done + main

--- NintendoProject/src/test/java/nintendo/test/Test.java
+++ NintendoProject/src/test/java/nintendo/test/Test.java
@@ -1,5 +1,9 @@
 package nintendo.test;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import nintendo.model.Adresse;
 import nintendo.model.Boutique;
 import nintendo.model.Client;
 import nintendo.model.Console;
@@ -9,6 +13,13 @@ public class Test {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		
+		Adresse adresse1 = new Adresse("67", "rue des capucines", "Blancherive");
+		Adresse adresse2 = new Adresse("23", "avenue de l'avenue", "Orgrimmar");
+		
+		Boutique boutique = new Boutique("MicroMania", adresse1);
+		Boutique boutique = new Boutique("FNAC", adresse2)
+		
 		Console nintendoDS = new Console("nintendoDS");
 		Console Wii = new Console("Wii");
 		Console Switch = new Console("Switch");
@@ -19,9 +30,18 @@ public class Test {
 		Jeu jeu4 = new Jeu("Metroid", nintendoDS);
 		Jeu jeu5 = new Jeu("Animal Crossing : New Horizon", Switch);
 		
+		List<Jeu> achatsJhon = new ArrayList<>();
+		achatsJhon.add(jeu1);
+		achatsJhon.add(jeu3);
+		
+		List<Jeu> achatsJordan = new ArrayList<>();
+		achatsJhon.add(jeu2);
+		achatsJhon.add(jeu5);
+		
 		Boutique boutique = new Boutique("Micromania"," 3 rue de Verdun");
 		Client c1 = new Client("Doe","John");
 		Client c2 = new Client("Abid","Jordan");
+		
 		
 	}
 
