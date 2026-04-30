06c4ab3c67e007ff73840db8b7b18d3e55c047d0
YohannLG01 <yohannleguen@hotmail.com>
2026-04-30 12:40:03 +0200

    Add prix , date dans Console

--- NintendoProject/src/main/java/nintendo/model/Console.java
+++ NintendoProject/src/main/java/nintendo/model/Console.java
@@ -3,9 +3,13 @@ package nintendo.model;
 public class Console {
 
 	private String nom;
+	private double prix;
+	private String date_sortie;
 
 	public Console(String nom) {
 		this.nom = nom;
+		this.prix = prix;
+		this.date_sortie = date_sortie;
 	}
 
 	public String getNom() {
@@ -18,8 +22,9 @@ public class Console {
 
 	@Override
 	public String toString() {
-		return "Console [nom=" + nom + "]";
+		return "Console [nom=" + nom + ", prix=" + prix + ", date_sortie=" + date_sortie + "]";
 	}
+
 	
 	
 }
